ad58ca5734dabb810cacb3a1de2298fefc2e4066
Alogyn <mohamed.derfoufi.dev@gmail.com>
2023-10-17 00:12:43 +0100

    debugg

--- 6-functions.c
+++ 6-functions.c
@@ -52,3 +52,83 @@ int print_address(va_list arg_lisr, char *buffer, unsigned int index)
 	free(hexa);
 	return (count + 2);
 }
+
+/**
+  * print_space - Prints integers with ' ' flag
+  *
+  * @arg_list: Input integers argument (int)
+  * @buffer: Buffer array (char)
+  * @index: Number of buffer index (int)
+  *
+  * Return: The length of an integer output
+ */
+
+int print_space_int(va_list arg_list, char *buffer, unsigned int index)
+{
+	int number;
+	unsigned int n, temp_num, i, diviser;
+
+	number = va_arg(arg_list, int);
+	if (number < 0)
+	{
+		n = number * -1;
+		index = _puts(buffer, '-', index);
+	}
+	else
+	{
+		n = number;
+		index = _puts(buffer, ' ', index);
+	}
+	temp_num = n;
+	diviser = 1;
+	while (temp_num > 9)
+	{
+		diviser *= 10;
+		temp_num /= 10;
+	}
+	for (i = 0; diviser > 0; diviser /= 10, i++)
+	{
+		index = _puts(buffer, ((n / diviser) % 10) + '0', index);
+	}
+	return (i + 1);
+}
+
+/**
+  * print_space_dig - Prints digits with ' ' flag
+  *
+  * @arg_list: Input digits argument (int)
+  * @buffer: Buffer array (char)
+  * @index: Number of buffer index (int)
+  *
+  * Return: The length of an digit output
+ */
+
+int print_space_dig(va_list arg_list, char *buffer, unsigned int index)
+{
+	int number;
+	unsigned int n, temp_num, i, diviser;
+
+	number = va_arg(arg_list, int);
+	if (number < 0)
+	{
+		n = number * -1;
+		index = _puts(buffer, '-', index);
+	}
+	else
+	{
+		n = number;
+		index = _puts(buffer, ' ', index);
+	}
+	temp_num = n;
+	diviser = 1;
+	while (temp_num > 9)
+	{
+		diviser *= 10;
+		temp_num /= 10;
+	}
+	for (i = 0; diviser > 0; diviser /= 10, i++)
+	{
+		index = _puts(buffer, ((n / diviser) % 10) + '0', index);
+	}
+	return (i + 1);
+}
--- 7-functions.c
+++ 7-functions.c
@@ -12,10 +12,9 @@
 
 int print_plus_int(va_list arg_list, char *buffer, unsigned int index)
 {
-	int number;
+	int number = va_arg(arg_list, int);;
 	unsigned int n, temp_num, i, diviser;
 
-	number = va_arg(arg_list, int);
 	if (number < 0)
 	{
 		n = number * -1;
@@ -52,10 +51,9 @@ int print_plus_int(va_list arg_list, char *buffer, unsigned int index)
 
 int print_plus_dig(va_list arg_list, char *buffer, unsigned int index)
 {
-	int number;
+	int number = va_arg(arg_list, int);;
 	unsigned int n, temp_num, i, diviser;
 
-	number = va_arg(arg_list, int);
 	if (number < 0)
 	{
 		n = number * -1;
@@ -92,11 +90,10 @@ int print_plus_dig(va_list arg_list, char *buffer, unsigned int index)
 
 int print_zero_oct(va_list arg_list, char *buffer, unsigned int index)
 {
-	int number, i, check_negative, count, n;
+	int number = va_arg(arg_list, int);
+	int i = 0, check_negative = 0, count, n = 0;
 	char *octal, *binary;
 
-	number = va_arg(arg_list, int);
-	check_negative = 0;
 	if (number == 0)
 	{
 		index = _puts(buffer, '0', index);
@@ -112,7 +109,7 @@ int print_zero_oct(va_list arg_list, char *buffer, unsigned int index)
 	binary = input_to_binary(binary, number, check_negative, 32);
 	octal = malloc(sizeof(char) * (11 + 1));
 	octal = input_to_octal(binary, octal);
-	for (n = i = count = 0; octal[i]; i++)
+	for (count = 0; octal[i]; i++)
 	{
 		if (octal[i] != '0' && n == 0)
 			n = 1;
@@ -139,11 +136,10 @@ int print_zero_oct(va_list arg_list, char *buffer, unsigned int index)
 
 int print_zero_hexa(va_list arg_list, char *buffer, unsigned int index)
 {
-	int number, i, check_negative, count, n;
+	int number = va_arg(arg_list, int);
+	int i = 0, check_negative = 0, count, n = 0;
 	char *hexa, *binary;
 
-	number = va_arg(arg_list, int);
-	check_negative = 0;
 	if (number == 0)
 	{
 		index = _puts(buffer, '0', index);
@@ -160,7 +156,7 @@ int print_zero_hexa(va_list arg_list, char *buffer, unsigned int index)
 	binary = input_to_binary(binary, number, check_negative, 32);
 	hexa = malloc(sizeof(char) * (8 + 1));
 	hexa = input_to_hexa(binary, hexa, 0, 8);
-	for (n = i = count = 0; hexa[i]; i++)
+	for (count = 0; hexa[i]; i++)
 	{
 		if (hexa[i] != '0' && n == 0)
 			n = 1;
@@ -187,11 +183,10 @@ int print_zero_hexa(va_list arg_list, char *buffer, unsigned int index)
 
 int print_zero_uhexa(va_list arg_list, char *buffer, unsigned int index)
 {
-	int number, i, check_negative, count, n;
+	int number = va_arg(arg_list, int);
+	int i = 0, check_negative = 0, count, n = 0;
 	char *hexa, *binary;
 
-	number = va_arg(arg_list, int);
-	check_negative = 0;
 	if (number == 0)
 	{
 		index = _puts(buffer, '0', index);
@@ -208,7 +203,7 @@ int print_zero_uhexa(va_list arg_list, char *buffer, unsigned int index)
 	binary = input_to_binary(binary, number, check_negative, 32);
 	hexa = malloc(sizeof(char) * (8 + 1));
 	hexa = input_to_hexa(binary, hexa, 1, 8);
-	for (n = i = count = 0; hexa[i]; i++)
+	for (count = 0; hexa[i]; i++)
 	{
 		if (hexa[i] != '0' && n == 0)
 			n = 1;
@@ -221,84 +216,4 @@ int print_zero_uhexa(va_list arg_list, char *buffer, unsigned int index)
 	free(binary);
 	free(hexa);
 	return (count + 2);
-}
-
-/**
-  * print_space - Prints integers with ' ' flag
-  *
-  * @arg_list: Input integers argument (int)
-  * @buffer: Buffer array (char)
-  * @index: Number of buffer index (int)
-  *
-  * Return: The length of an integer output
- */
-
-int print_space_int(va_list arg_list, char *buffer, unsigned int index)
-{
-	int number;
-	unsigned int n, temp_num, i, diviser;
-
-	number = va_arg(arg_list, int);
-	if (number < 0)
-	{
-		n = number * -1;
-		index = _puts(buffer, '-', index);
-	}
-	else
-	{
-		n = number;
-		index = _puts(buffer, ' ', index);
-	}
-	temp_num = n;
-	diviser = 1;
-	while (temp_num > 9)
-	{
-		diviser *= 10;
-		temp_num /= 10;
-	}
-	for (i = 0; diviser > 0; diviser /= 10, i++)
-	{
-		index = _puts(buffer, ((n / diviser) % 10) + '0', index);
-	}
-	return (i + 1);
-}
-
-/**
-  * print_space_dig - Prints digits with ' ' flag
-  *
-  * @arg_list: Input digits argument (int)
-  * @buffer: Buffer array (char)
-  * @index: Number of buffer index (int)
-  *
-  * Return: The length of an digit output
- */
-
-int print_space_dig(va_list arg_list, char *buffer, unsigned int index)
-{
-	int number;
-	unsigned int n, temp_num, i, diviser;
-
-	number = va_arg(arg_list, int);
-	if (number < 0)
-	{
-		n = number * -1;
-		index = _puts(buffer, '-', index);
-	}
-	else
-	{
-		n = number;
-		index = _puts(buffer, ' ', index);
-	}
-	temp_num = n;
-	diviser = 1;
-	while (temp_num > 9)
-	{
-		diviser *= 10;
-		temp_num /= 10;
-	}
-	for (i = 0; diviser > 0; diviser /= 10, i++)
-	{
-		index = _puts(buffer, ((n / diviser) % 10) + '0', index);
-	}
-	return (i + 1);
 }
